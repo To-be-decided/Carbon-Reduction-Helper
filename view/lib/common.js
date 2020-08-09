b76f6e1cbc2f89a593b484a7ac09751eaef3919d
Rob <robert.sharpe@evergreen-consulting.co.uk>
2020-08-09 08:23:15 +0100

    Convert tips from table to div. Move Google analytics to common.js. Tidied up order of elements. Tidied up file includes.

--- view/lib/common.js
+++ view/lib/common.js
@@ -1,3 +1,17 @@
+// Google analytics
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'UA-173546127-1');
+    window.dataLayer = window.dataLayer || [];
+
+
+// Common parameters
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 var step = urlParams.get('step')
