8400e3af330f292d62511e2722853290680702cb
Rob <robert.sharpe@evergreen-consulting.co.uk>
2020-08-10 07:51:10 +0100

    Add controller_init() and model_init Separate config from settings. Revert back from using numeric state ids to original text state ids Use persistence.js code. Add model test harness.

--- model.js
+++ model.js
@@ -1,22 +1,15 @@
-/* Step state values
-@todo Step state values need to be converted to numeric stateIds, so that state text changes only need to be in one place.
-*/
+/* Step state values */
 stepStateId_undefined   = ""
-stepStateId_maybe       = "maybe"     // Maybe
-stepStateId_planTo      = "aspiring"  // Plan to
-stepStateId_completed   = "done"      // Completed
-stepStateId_na          = "na"        // Not applicable
-stepStateId_never       = "never"     // Never
 
 /**
  * @summary
  * @type {({summary: string, embedUrl: string, editUrl: string, defaultState: string, stepId: number}|{summary: string, embedUrl: string, editUrl: string, defaultState: string, stepId: number}|{summary: string, embedUrl: string, editUrl: string, defaultState: string, stepId: number}|{summary: string, embedUrl: string, editUrl: string, defaultState: string, stepId: number}|{summary: string, embedUrl: string, editUrl: string, defaultState: string, stepId: number})[]}
  */
-let stepMoreInfoData = [
+let stepEntriesConfig = [
     {
         "stepId": 10,
         "summary": "Replace gas boiler with an air source heat pump",
-        "defaultState": stepStateId_planTo,
+        "defaultState": "aspiring",
         "embedUrl": "https://docs.google.com/document/d/e/2PACX-1vSU47SJDExoBZlMuUMcKxciv-G4TCcy-LLFG6WLu2o5TJb88uQ7RVbq9x0MVrvwII04DwcB5lNdVZ4G/pub?embedded=true",
         "editUrl": "https://docs.google.com/document/u/0/d/1pK1G4sH_IXMSGDAxZJyifKp77WlhjTPUtZSaplVpuWk/edit",
     },
@@ -30,63 +23,95 @@ let stepMoreInfoData = [
     {
         "stepId": 30,
         "summary": "Replace 5 beef (herd) portions per week with poultry",
-        "defaultState":  stepStateId_planTo,
+        "defaultState":  "aspiring",
         "embedUrl": "https://docs.google.com/document/d/e/2PACX-1vRy_pvL4djiznmVE1iGbxjx3p4N9LZ-BQK1egCcSdqakIm6sUz2n6WGY6nCPzQoGBqvJrULlN7gITep/pub?embedded=true",
         "editUrl": "https://docs.google.com/document/d/189TdeSvhg5xnG05ojybBP0NcSgevV0SCDQtf_X9Hwg8/edit"
     },
     {
         "stepId": 40,
         "summary": "Replace a petrol car with an electric car",
-        "defaultState":  stepStateId_completed,
+        "defaultState":  "done",
         "embedUrl": "https://docs.google.com/document/d/e/2PACX-1vQLByAy2Xh2C6aE6wYmgQ70WKHrZTneL23BfIcFQLb7UE-LBfRkTy88KzDKUsPZ5WqSNGpO5JxQRD6P/pub?embedded=true",
         "editUrl": "https://docs.google.com/document/d/1LpWNo4Q592zBO01xp8IzYpWbKVovOPSReDV9vv598Hc/edit"
     },
     {
         "stepId": 50,
-        "defaultState":  stepStateId_planTo,
+        "defaultState":  "aspiring",
         "summary": "Increase loft insulation from ??cm to ??cm in a gas heated house with an old boiler",
         "embedUrl": "https://docs.google.com/document/d/e/2PACX-1vSQYSwJwsj4zICvkhOX-rQB6Cyt-xZQMUVNzq8glFSTsI6dT-0zL6mbgXEVNXwexj7NEAUxqRfpUC-0/pub?embedded=true",
         "editUrl": "https://docs.google.com/document/d/1hKaGpUCPWt0OCTXfa-uaqnpzu7YmqwJRUqDVR9u5y1Q/edit",
     },
     {
         "stepId": 60,
-        "defaultState":  stepStateId_planTo,
+        "defaultState":  "aspiring",
         "summary": "Replace old gas boiler with a new condensing boiler",
         "embedUrl": "https://docs.google.com/document/d/e/2PACX-1vRkLE8vg0CcJEYu8-xAtkjD8bXBCv4BXpljMxxDBzlAuXqszDafnyrNY5Vtsm01iTya0rWpqwuquI_C/pub?embedded=true",
         "editUrl": "https://docs.google.com/document/d/1pfhk3K2M_Y3FNotjdDzGpXftBilaDkJ5zqYS5Le2VtA/edit#",
     }
 ]
 
+let settings = [
+    {"stepId": 10, "stepStateId": ""},
+    {"stepId": 20, "stepStateId": "test"},
+    {"stepId": 30, "stepStateId": ""},
+    {"stepId": 40, "stepStateId": ""},
+    {"stepId": 50, "stepStateId": ""},
+    {"stepId": 60, "stepStateId": ""},
+    {"stepId": 70, "stepStateId": ""},
+    {"stepId": 80, "stepStateId": ""},
+    {"stepId": 90, "stepStateId": ""},
+    {"stepId": 100, "stepStateId": ""},
+    {"stepId": 110, "stepStateId": ""},
+    {"stepId": 120, "stepStateId": ""},
+    {"stepId": 130, "stepStateId": ""},
+    {"stepId": 140, "stepStateId": ""},
+    {"stepId": 150, "stepStateId": ""},
+    {"stepId": 160, "stepStateId": ""},
+    {"stepId": 170, "stepStateId": ""},
+    {"stepId": 180, "stepStateId": ""},
+    {"stepId": 190, "stepStateId": ""},
+    {"stepId": 200, "stepStateId": ""},
+    {"stepId": 210, "stepStateId": ""},
+    {"stepId": 220, "stepStateId": ""},
+    {"stepId": 230, "stepStateId": ""},
+    {"stepId": 240, "stepStateId": ""},
+    {"stepId": 250, "stepStateId": ""},
+    {"stepId": 260, "stepStateId": ""}
+]
+
 let volatilityConfirmed = false;
 
 model_init = function(){
+    // Read settings from cookie
+    console.log("model_init(): All cookies = " + document.cookie)
+
+    loadValues()
 }
 
 model_setStepState = function (stepId, stateId) {
     console.log("Setting status for step " + stepId + " as " + stateId)
 
-    if (!volatilityConfirmed) {
-        alert('Warning: Step selections are not be saved in this proof-of-concept or updated on the dashboard.');
-        volatilityConfirmed = true;
-    }
+    let index = find_stepSettingsIndexByStepId(stepId)
+    if(index === undefined) return stepStateId_undefined
+
+    settings[index].stepStateId = stateId
+    saveValues();
+    console.log("Saved")
 }
+
 
 /**
  * @summary Get the current step state for the given stepId
  * @param stepId
  * @returns {string}
  */
-model_getStepState = function (stepId) {
+model_getStepStateId = function (stepId) {
     if(stepId === "") return stepStateId_undefined;
 
-    /* Find stepId in moreInfoData */
-    let moreInfoDataEntry = stepMoreInfoData.find(
-        stepEntry => stepEntry.stepId == stepId,
-        stepId
-    )
-    if(moreInfoDataEntry === undefined) return stepStateId_undefined
+    let index = find_stepSettingsIndexByStepId(stepId)
+    if(index === undefined) return stepStateId_undefined
 
-    return moreInfoDataEntry.defaultState
+    return settings[index].stepStateId
 }
 
 
@@ -99,7 +124,7 @@ model_getStepEmbedUrl = function(stepId){
     if(stepId === "") return "";
 
     /* Find stepId in moreInfoData */
-    let moreInfoDataEntry = stepMoreInfoData.find(
+    let stepEntryConfig = stepEntriesConfig.find(
         stepEntry => stepEntry.stepId == stepId,
         stepId
     )
@@ -117,12 +142,26 @@ model_getStepEmbedUrl = function(stepId){
 model_getStepEditUrl = function(stepId){
     if(stepId === "") return "";
 
-    /* Find stepId in moreInfoData */
-    let moreInfoDataEntry = stepMoreInfoData.find(
+    let index = find_stepConfigIndexByStepId(stepId)
+    if(index === undefined) return ""
+
+    return stepEntriesConfig[index].editUrl
+}
+
+// ============================================
+
+find_stepSettingsIndexByStepId = function(stepId){
+    let index = settings.findIndex(
         stepEntry => stepEntry.stepId == stepId,
         stepId
     )
-    if(moreInfoDataEntry === undefined) return ""
+    return index
+}
 
-    return moreInfoDataEntry.editUrl
+find_stepConfigIndexByStepId = function(stepId){
+    let index = stepEntriesConfig.findIndex(
+        stepEntry => stepEntry.stepId == stepId,
+        stepId
+    )
+    return index
 }
