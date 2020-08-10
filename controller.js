fe184f4baa2e1ad24f2f33723bb069491eb62ba0
Rob <robert.sharpe@evergreen-consulting.co.uk>
2020-08-10 15:27:47 +0100

    Change model_setStepState() to model_setStepStateId() Rationalised MVC:    Finished and tested persistence.js code    Add remaining step info entries to model.js    Add model_reload()    Add reduction value to stepEntriesConfig    Add test_controller.html    Add model_getStepReduction()    Rename moreInfoDataEntry to stepEntryConfig    Add yourSavings() to controller.js

--- controller.js
+++ controller.js
@@ -1,8 +1,36 @@
+var savedDoneThresholdPerc = 20;
+
 controller_init = function(){
     model_init()
     view_init()
 }
 
 controller_handle_stepStateChange = function(stepId, stateId){
-    model_setStepState(stepId, stateId)
+    model_setStepStateId(stepId, stateId)
+}
+
+yourSavings = function(){
+    let total_complete = 0
+    let total_planned = 0
+
+    settings.forEach(
+        stepEntry => {
+            switch(stepEntry.stepStateId){
+                case "done":
+                    total_complete = total_complete +
+                        stepEntriesConfig[find_stepConfigIndexByStepId(stepEntry.stepId)].reduction
+                    break
+
+                case "aspiring":
+                    total_planned = total_planned +
+                        stepEntriesConfig[find_stepConfigIndexByStepId(stepEntry.stepId)].reduction
+                    break;
+            }
+        }
+    )
+
+    return {
+        "completed": total_complete,
+        "planned": total_planned
+    }
 }
--- persistence.js
+++ persistence.js
@@ -1,32 +1,26 @@
-function saveValues() {
-
-    let allSelects = document.querySelectorAll('.table select');
-    let setSelects = {};
-
-    allSelects.forEach((s) => { 
-        if (s.value != "") {
-             console.log(s.id);
-             setSelects[s.id] = s.value;
-        }});
-
+saveValues = function(){
     let expire = new Date();
-    expire.setTime(new Date().getTime() + 3600000*24*14);
-    document.cookie = "savedValues=" + JSON.stringify(setSelects) + ";expires="+expire.toGMTString();     
+
+    let cookieValue = JSON.stringify(settings)
+    var date = new Date("Februari 10, 2099");
+    var dateString = date.toGMTString();
+
+    document.cookie = "crh_settings=" + cookieValue + ";expires="+dateString;
+
+    // @todo Check that cookie was saved correctly
+    if(document.cookie == undefined || document.cookie.search("crh_settings=" + cookieValue)==0){
+        alert("Could not save changes")
+    }
 }
 
-
 function loadValues() {
+    let crh_cookiePair = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('crh_settings'))
 
-    const setSelects = JSON.parse(document.cookie
-      .split('; ')
-      .find(row => row.startsWith('savedValues'))
-      .split('=')[1]);    
-    
-    let allSelects = document.querySelectorAll('.table select');
-    
-    allSelects.forEach((s) => { 
-        if (setSelects.hasOwnProperty(s.id)) {
-             s.value = setSelects[s.id];
-        }
-    });
+    if(crh_cookiePair == undefined){
+        console.log("model_init(): CRH Cookie not found continuing with default values")
+    }else{
+        settings = JSON.parse(crh_cookiePair.split('=')[1]);
+    }
 }
